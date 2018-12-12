140c57ad97c7033adbac2ec4be56b7e2a946cc54
subhashadaps <45817030+subhashadaps@users.noreply.github.com>
2018-12-12 02:00:43 -0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello, World!" ); 
+		System.out.println("Today is June 13, 2016. I am alive!");
+		System.out.println("My name is Josh!");
+	}
+
+}
---
+++ ReverseNumberDemo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class ReverseNumberDemo
+{
+   public static void main(String args[])
+   {
+      int num=123456789;
+	//Scanner scan = new Scanner(System.in);
+	System.out.println("Enter any number:");
+	//int num = scan.nextInt();
+    	
+      System.out.println("Given specified number is: "+num);
+      int reversenum =0;
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+    
+      System.out.println("Reverse of specified number is: "+reversenum);
+   }
+}
